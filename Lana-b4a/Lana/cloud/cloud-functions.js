b8bf5c02305c1358800aa7cdfff1f17dd43eb387
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-06-18 22:58:57 -0300

    Add new cloud functions for Context and refracts cloud/main.js.

---
+++ Lana-b4a/Lana/cloud/cloud-functions.js
@@ -0,0 +1,155 @@
+const destroyAllSessions = () => {
+    let query = new Parse.Query(Parse.Session);
+  
+    query.find({
+      success: (results) => {
+        for (let session of results) {
+          session.destroy({useMasterKey: true});
+        }
+        return true;
+      },
+      error: (error) => {
+        return false;
+      },
+      useMasterKey: true
+    });
+  };
+
+module.exports.echo = (Parse) => { 
+    return (request, response) => {
+    response.success({ code: 200, message: request.params.string });
+  };
+};
+
+module.exports.getUserByField = (Parse) => { 
+    return (request, response) => {
+    let userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo(request.params.field, request.params.value);
+    userQuery.first({
+      success: (user) => {
+        if(user != undefined)
+          response.success({ code: 200, message: user.toJSON() });
+        else
+          response.error(404, "Usuario não encontrado");
+      },
+      error: (err) => {
+        response.error(404, "Usuario não encontrado");
+      }
+    });
+  };
+};
+
+module.exports.setUserField = (Parse) => {
+    return (request, response) => {
+    let userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo("objectId", request.params.userId);
+    userQuery.first()
+      .then(userToUpdate => {
+        if(userToUpdate !== undefined) {
+          userToUpdate.set(request.params.field, request.params.value);
+          userToUpdate.save(null, {useMasterKey:true})
+          .then(userToUpdate => {
+            response.success({ code: 200, message: userToUpdate.toJSON() });
+          })
+          .catch(err => {
+            response.error(500, "Erro ao modificar Usuario");
+          });
+        }
+        else
+          response.error(404, "Usuario não encontrado");
+      })
+      .catch(err => {
+        response.error(404, "Usuario não encontrado");
+      });
+  };
+};
+
+module.exports.registerUser = (Parse) => {
+    return (request, response) => {
+    if(request.params.username == undefined ||
+      request.params.password == undefined ||
+      request.params.interface == undefined ||
+      request.params.interfaceId == undefined ||
+      request.params.name == undefined) {
+        response.error(400, "Usuario precisa de username, password, interface, interfaceId e name");
+    }
+    let user = new Parse.User();
+    user.set("username", request.params.username);
+    user.set("password", request.params.password);
+    user.set(request.params.interface, request.params.interfaceId);
+    user.set("name", request.params.name);
+  
+    user.signUp(null)
+      .then((newUser) => {
+        destroyAllSessions();
+        response.success({ code: 200, message: newUser });
+      })
+      .catch((newUser, err) => response.error(409, "Usuario já existente"));
+  };
+};
+
+module.exports.loginUser = (Parse) => {
+    return (request, response) => {
+    Parse.User.logIn(request.params.username, request.params.password)
+      .then((user) => {
+        destroyAllSessions();
+        response.success({ code: 200, message: user.toJSON() });
+      })
+      .catch((user, err) => response.error(401, "Usuario/Senha Incorreta"));
+  };
+};
+
+module.exports.setContext = (Parse) => {
+    return (request, response) => {
+    if(request.params.interface == undefined ||
+      request.params.interfaceId == undefined ||
+      (request.params.intents == undefined && request.params.entities == undefined)) {
+        response.error(400, "Context precisa de interface, interfaceId, intents e/ou entities");
+    }
+    let Context = Parse.Object.extend('Context');
+    let contextQuery = new Parse.Query(Context);
+    let myContext = new Context();
+    contextQuery.equalTo(request.params.interface, request.params.interfaceId);
+    contextQuery.first()
+      .then(contextToUpdate => {
+        if(contextToUpdate != undefined)
+          myContext = contextToUpdate;
+        else
+          myContext.set(request.params.interface, request.params.interfaceId);
+  
+        if(request.params.intents != undefined)
+          myContext.set("Intents", request.params.intents);
+        if(request.params.entities != undefined)
+          myContext.set("Entities", request.params.entities);
+      
+        myContext.save(null)
+          .then(newContext => {
+            response.success({code: 200, message: newContext});
+          })
+          .catch(err => {
+            response.error(500, "erro ao salvar Context");
+          });
+      })
+      .catch(err => {
+        response.error(500, "erro ao salvar Context");
+      });
+  };
+};
+
+module.exports.getContextByInterfaceId = (Parse) => {
+    return (request, response) => {
+    let Context = Parse.Object.extend('Context');
+    let contextQuery = new Parse.Query(Context);
+    contextQuery.equalTo(request.params.interface, request.params.interfaceId);
+    contextQuery.first()
+      .then(contextToFind => {
+        if(contextToFind != undefined)
+          response.success({code: 200, message: contextToFind.toJSON()});
+        else
+          response.error(404, "Context não encontrado");
+      })
+      .catch(err => {
+        response.error(500, "erro ao buscar Context");
+      });
+  };
+};
--- Lana-b4a/Lana/cloud/main.js
+++ Lana-b4a/Lana/cloud/main.js
@@ -1,92 +1,15 @@
-Parse.Cloud.define("echo", (request, response) => {
-  response.success({ code: 200, message: request.params.string });
-});
+const cloudFunctions = require('./cloud-functions');
 
-const destroyAllSessions = () => {
-  let query = new Parse.Query(Parse.Session);
+Parse.Cloud.define("echo", cloudFunctions.echo(Parse));
 
-  query.find({
-    success: (results) => {
-      for (let session of results) {
-        session.destroy({useMasterKey: true});
-      }
-      return true;
-    },
-    error: (error) => {
-      return false;
-    },
-    useMasterKey: true
-  });
-};
+Parse.Cloud.define("getUserByField", cloudFunctions.getUserByField(Parse));
 
-Parse.Cloud.define("getUserByField", (request, response) => {
-  let userQuery = new Parse.Query(Parse.User);
-  userQuery.equalTo(request.params.field, request.params.value);
-  userQuery.first({
-    success: (user) => {
-      if(user != undefined)
-        response.success({ code: 200, message: user.toJSON() });
-      else
-        response.error(404, "Usuario não encontrado");
-    },
-    error: (err) => {
-      response.error(404, "Usuario não encontrado");
-    }
-  });
-});
+Parse.Cloud.define("setUserField", cloudFunctions.setUserField(Parse));
 
+Parse.Cloud.define("registerUser", cloudFunctions.registerUser(Parse));
 
-Parse.Cloud.define("setUserField", (request, response) => {
-  let userQuery = new Parse.Query(Parse.User);
-  userQuery.equalTo("objectId", request.params.userId);
-  userQuery.first()
-    .then(userToUpdate => {
-      if(userToUpdate !== undefined) {
-        userToUpdate.set(request.params.field, request.params.value);
-        userToUpdate.save(null, {useMasterKey:true})
-        .then(userToUpdate => {
-          response.success({ code: 200, message: userToUpdate.toJSON() });
-        })
-        .catch((err) => {
-          response.error(500, "Erro ao modificar Usuario");
-        });
-      }
-      else
-        response.error(404, "Usuario não encontrado");
-    })
-    .catch((err) => {
-      response.error(404, "Usuario não encontrado");
-    });
-});
+Parse.Cloud.define("loginUser", cloudFunctions.loginUser(Parse));
 
+Parse.Cloud.define("setContext", cloudFunctions.setContext(Parse));
 
-Parse.Cloud.define("registerUser", (request, response) => {
-  if(request.params.username == undefined ||
-    request.params.password == undefined ||
-    request.params.interface == undefined ||
-    request.params.interfaceId == undefined ||
-    request.params.name == undefined) {
-      response.error(400, "Usuario precisa de username, password, interface, interfaceId e name");
-  }
-  let user = new Parse.User();
-  user.set("username", request.params.username);
-  user.set("password", request.params.password);
-  user.set(request.params.interface, request.params.interfaceId);
-  user.set("name", request.params.name);
-
-  user.signUp(null)
-    .then((newUser) => {
-      destroyAllSessions();
-      response.success({ code: 200, message: newUser });
-    })
-    .catch((newUser, err) => response.error(409, "Usuario já existente"));
-});
-
-Parse.Cloud.define("loginUser", (request, response) => {
-  Parse.User.logIn(request.params.username, request.params.password)
-    .then((user) => {
-      destroyAllSessions();
-      response.success({ code: 200, message: user.toJSON() });
-    })
-    .catch((user, err) => response.error(401, "Usuario/Senha Incorreta"));
-});
+Parse.Cloud.define("getContextByInterfaceId", cloudFunctions.getContextByInterfaceId(Parse));
