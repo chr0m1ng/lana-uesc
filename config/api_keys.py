52bec3559142142b07545d88b16941b9ceb63497
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-08-06 19:40:18 -0300

    more on github

---
+++ config/api_keys.py
@@ -0,0 +1,6 @@
+# coding=utf8
+#### Keys liberadas para usar o bothub [key] = proprietario
+KEYS = {
+    '863D951A-E27C-4D68-BD30-E3FB605BF1B9' : 'lana(Gabriel Santos)',
+    '55067DB2-C6CE-4585-86D7-9164A01BF8D1' : 'postman(Gabriel Santos)'
+}
